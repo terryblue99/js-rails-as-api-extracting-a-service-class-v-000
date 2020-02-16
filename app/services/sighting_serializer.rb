c418b60ca79acb8147ddb4d193dba593466dba8d
Terry Blue <terryblue@comcast.net>
2020-02-16 01:47:17 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -4,11 +4,24 @@ class SightingSerializer
     @sighting = sighting_object
   end
 
+  # def to_serialized_json
+  #   @sighting.to_json(:include => {
+  #     :bird => {:only => [:name, :species]},
+  #     :location => {:only => [:latitude, :longitude]}
+  #   }, :except => [:updated_at])
+  # end
   def to_serialized_json
-    @sighting.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
+    options = {
+      include: {
+        bird: {
+          only: [:name, :species]
+        },
+        location: {
+          only: [:latitude, :longitude]
+        }
+      },
+      except: [:updated_at],
+    }
+    @sighting.to_json(options)
   end
-
 end
