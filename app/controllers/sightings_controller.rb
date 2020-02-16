52ecd645452ac8e9ba3193fff4868b773320c661
Terry Blue <terryblue@comcast.net>
2020-02-16 01:40:35 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,7 +2,7 @@ class SightingsController < ApplicationController
     def show
         @sighting = Sighting.find(params[:id])
         render json: @sighting.to_json(:include => {
-          :bird => {:only =>[:name, :species]}, 
+          :bird => {:only =>[:name, :species]},
           :location => {:only =>[:latitude, :longitude]}
         }, :except => [:updated_at])
     end
